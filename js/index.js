3c4c92ec04f0ff79bf664e93edd721a8c6146a25
DIEGO GONCALVEZ <diego.data.icloud@gmail.com>
2021-08-10 23:22:46 -0300

    agregue hoja de .js

---
+++ js/index.js
@@ -0,0 +1,20 @@
+const $video = document.querySelector('#video')
+const $play = document.querySelector('#play')
+const $pause = document.querySelector('#pause')
+
+$play.addEventListener('click', handlePlay)
+$pause.addEventListener('click', handlePause)
+
+function handlePlay() {
+    $video.play()
+    $play.hidden = true
+    $pause.hidden = false
+    console.log('le diste click al boton de play')
+}
+
+function handlePause() {
+    $video.pause()
+    $pause.hidden = true
+    $play.hidden = false
+    console.log('le diste click al boton de pause')
+}
