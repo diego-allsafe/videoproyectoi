a76a88478dbe35201ca76e71a7bb1bbcd1e79abf
DIEGO GONCALVEZ <diego.data.icloud@gmail.com>
2021-08-14 10:50:58 -0300

    finalice la barra de avance

--- js/index.js
+++ js/index.js
@@ -49,3 +49,10 @@ function handleTimeUpdate(){
     $progress.value = $video.currentTime
     //console.log('tiempo actual', $video.currentTime)
 }
+
+$progress.addEventListener('input', handleInput)
+
+function handleInput() {
+    $video.currentTime = $progress.value
+    console.log($progress.value)
+}
